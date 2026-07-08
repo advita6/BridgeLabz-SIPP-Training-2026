e569df64b346aa7c5424858d47e0145158c0a192
Advita Singh <advita.singh_cs.ccv23@gla.ac.in>
2026-07-09 01:07:05 +0530

    Keep only SQL assignments in SQL branch

--- core-java-practice/gcr-codebase/recursion/Assignment1/calculateFactorial.java
+++
@@ -1,8 +0,0 @@
-public static int factorial(int n) {
-    // Base Case
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    // Recursive Step
-    return n * factorial(n - 1);
-}
--- core-java-practice/gcr-codebase/recursion/Assignment1/reverseString.java
+++
@@ -1,8 +0,0 @@
-public static String reverseString(String str) {
-    // Base Case
-    if (str == null || str.length() <= 1) {
-        return str;
-    }
-    // Recursive Step: Last character + reverse of remaining substring
-    return str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
-}
